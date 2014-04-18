8ce7c3053b9c7011944ef96ccce05d2d6de28471
Ash Wilson <smashwilson@gmail.com>
2014-04-18 09:01:32 -0400

    Enable noisy locale failures.
    Yes, mainly to shut that ActiveRecord warning up. :wink:

--- lib/peril.rb
+++ lib/peril.rb
@@ -3,6 +3,9 @@ require 'active_record'
 require_relative 'peril/config'
 require_relative 'peril/event'
 
+# Fail loudly if the a locale is unavailable.
+I18n.config.enforce_available_locales = true
+
 module Peril
 
   def self.table_name_prefix ; '' ; end
