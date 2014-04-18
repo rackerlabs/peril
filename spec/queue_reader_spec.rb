bbe44adec8a702ce518b5da4d5f85ae9ca2490f4
Ash Wilson <smashwilson@gmail.com>
2014-04-18 10:45:35 -0400

    Spec for a `poll` method.

--- spec/queue_reader_spec.rb
+++ spec/queue_reader_spec.rb
@@ -36,13 +36,25 @@ describe QueueReader do
     end
   end
 
-  it 'scans a Cloud Queue for events' do
-    q = qr.find_or_create_queue
-    0.upto(5) do |i|
-      q.enqueue "{ \"unique_id\": \"#{i}#{i}\", \"reporter\": \"minitest\" }", 3600
+  describe 'with events waiting' do
+    before do
+      q = qr.find_or_create_queue
+      0.upto(5) do |i|
+        q.enqueue "{ \"unique_id\": \"#{i}#{i}\", \"reporter\": \"minitest\" }", 3600
+      end
     end
 
-    es = qr.scan
-    es.map(&:unique_id).must_equal %w{00 11 22 33 44 55}
+    it 'scans a Cloud Queue for events' do
+      qr.scan.map(&:unique_id).must_equal %w(00 11 22 33 44 55)
+    end
+
+    it 'polls a Cloud Queue for events' do
+      seen = []
+      qr.poll do |event|
+        seen << event
+        :stop if seen.size == 5
+      end
+      seen.map(&:unique_id).must_equal %w(00 11 22 33 44 55)
+    end
   end
 end
