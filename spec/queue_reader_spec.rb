9c04b37c61f31f67e74007124d9717b524744276
Ash Wilson <smashwilson@gmail.com>
2014-04-18 09:52:11 -0400

    Test for finding an existing queue.

--- spec/queue_reader_spec.rb
+++ spec/queue_reader_spec.rb
@@ -1,9 +1,23 @@
 require_relative 'spec_helpers'
 
 describe QueueReader do
-  it 'polls a Cloud Queue for events' do
-    qr = QueueReader.new
+  let(:qr) { QueueReader.new }
+  let(:config) { Peril::Config.get }
 
+  it 'finds an existing Cloud Queue' do
+    puts config.inspect
+    service = Fog::Rackspace::Queues.new(
+      rackspace_username: config['rackspace']['username'],
+      rackspace_api_key: config['rackspace']['api_key']
+    )
+
+    existing = service.queues.create name: config['queue_name']
+
+    found = qr.find_or_create_queue
+    found.must_be_same_as existing
+  end
+
+  it 'polls a Cloud Queue for events' do
     q = qr.find_or_create_queue
     0.upto(5) do |i|
       q.enqueue "{ \"unique_id\": \"#{i}#{i}\", \"reporter\": \"minitest\" }", 3600
