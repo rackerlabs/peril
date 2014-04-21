bde105884d4d380ce0d9060a2ed9ae1e4bd1fb84
Ash Wilson <smashwilson@gmail.com>
2014-04-21 11:43:20 -0400

    Main entry point.

---
+++ lib/peril/main.rb
@@ -0,0 +1,24 @@
+module Peril
+
+  # Entry point for the CLI. Configure all the things, then poll the Cloud
+  # Queue and fire all notifiers each time an Event arrives.
+  #
+  class Main
+    include Loggable
+
+    # Start the loop.
+    #
+    def go
+      config = Peril::Config.get
+      config.dbconnect!
+
+      logger.fatal "LOG DAMN YOU"
+
+      qr = QueueReader.new
+      qr.find_or_create_queue
+      qr.poll do |event|
+        logger.info "Got: #{event.inspect}"
+      end
+    end
+  end
+end
