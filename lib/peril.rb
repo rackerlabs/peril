9d20133cead656d4d684d54de40666c3759218ad
Ash Wilson <smashwilson@gmail.com>
2014-04-21 15:14:52 -0400

    Notifier framework.

--- lib/peril.rb
+++ lib/peril.rb
@@ -6,6 +6,7 @@ I18n.config.enforce_available_locales = true
 
 require_relative 'peril/config'
 require_relative 'peril/loggable'
+require_relative 'peril/notifier'
 
 require_relative 'peril/event'
 require_relative 'peril/incident'
---
+++ spec/notifier_spec.rb
@@ -0,0 +1,34 @@
+require_relative 'spec_helpers'
+
+class FakeNotifier < Notifier
+  attr_accessor :called
+  attr_accessor :event, :incident
+
+  def process(event, incident)
+    @event, @incident = event, incident
+  end
+end
+
+describe Notifier do
+  before { Notifier.clear }
+
+  it 'registers itself with Notifier.known' do
+    FakeNotifier.register
+    Notifier.known.count { |n| FakeNotifier === n }.must_equal 1
+  end
+
+  it 'registers with a block' do
+    FakeNotifier.register { |n| n.called = true }
+    n = Notifier.known.detect { |n| FakeNotifier === n }
+    n.must_be :called
+  end
+
+  it 'notifies all registered instances' do
+    FakeNotifier.register
+    n = Notifier.known.detect { |n| FakeNotifier === n }
+
+    FakeNotifier.handle 1, 2
+    n.event.must_equal 1
+    n.incident.must_equal 2
+  end
+end
