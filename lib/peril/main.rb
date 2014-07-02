8364e6a2dc6b566e6c85db247f984bd3b99a77a7
Ash Wilson <smashwilson@gmail.com>
2014-07-02 15:51:38 -0400

    Rework Main to use rufus to drive everything.

--- lib/peril/main.rb
+++ lib/peril/main.rb
@@ -1,3 +1,5 @@
+require 'rufus-scheduler'
+
 module Peril
 
   # Entry point for the CLI. Configure all the things, then poll the Cloud
@@ -10,17 +12,30 @@ module Peril
     #
     def go
       config = Peril::Config.get
+
+      logger.debug 'establishing database connection'
       config.dbconnect!
 
+      logger.debug 'activating notifiers'
       Notifier.activate
+      logger.debug "active notifier count: #{Notifier.known.size}"
 
-      qr = QueueReader.new
-      qr.find_or_create_queue
-      qr.poll do |event|
-        logger.debug "Processing: #{event.inspect}"
-        incident = Incident.for_event(event)
-        Notifier.handle(event, incident)
+      logger.debug 'activating slurpers'
+      Slurper.activate
+      logger.debug "active slurper count: #{Slurper.known.size}"
+
+      scheduler = Rufus::Scheduler.new
+
+      logger.info 'all systems go. commence primary ignition'
+      scheduler.every config.default(:poll_time, '5m') do
+        Slurper.poll do |event|
+          logger.debug "Processing: #{event.inspect}"
+          incident = Incident.for_event(event)
+          Notifier.handle(event, incident)
+        end
       end
+
+      scheduler.join
     end
   end
 end
