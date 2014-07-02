bd5a6c5bd6e1034e5ffa6f976a8b709d0e6b333f
Ash Wilson <smashwilson@gmail.com>
2014-07-02 14:58:03 -0400

    Rewrite Hubslurp as a Slurper subclass.

--- lib/peril/slurpers/hubslurp.rb
+++ lib/peril/slurpers/hubslurp.rb
@@ -1,139 +1,78 @@
-#!/usr/bin/env ruby
-
-require 'fog'
 require 'octokit'
-require 'rufus-scheduler'
-require 'log4r'
-require 'yaml'
 
-# Constants
+module Peril
+  module Slurpers
 
-CONFIG_PATH = File.join __dir__, 'config.yml'
-VERSION_PATH = File.join __dir__, 'VERSION'
-LAST_PATH = File.join __dir__, '.last_check'
+    class Hubslurp < Slurper
+      include Loggable
 
-# Support
+      attr_accessor :access_token, :repositories
+      attr_accessor :last_check
 
-class Repository
-  attr_reader :name, :tags
+      Repository = Struct.new(:name, :tags, :pattern)
 
-  def initialize(name, tags)
-    @name, @tags = name, tags
-  end
+      def initialize
+        @repositories = []
 
-  def self.from_h(hash)
-    unless hash['name']
-      raise RuntimeError.new("Malformed repository hash: #{hash.inspect}")
-    end
-    new(hash['name'], hash['tags'] || [])
-  end
-end
+        # Start checking a day ago.
+        @last_check = Time.now - 86400
+      end
 
-# Configuration
+      def repository(name, tags = [], keywords = [])
+        if keywords.empty?
+          pattern = nil
+        else
+          pattern = Regexp.new(
+            keywords.map { |kw| Regexp.escape kw }.join('|'),
+            Regexp::IGNORECASE
+          )
+        end
 
-def configure
-  @config = YAML.load_file(CONFIG_PATH)
-  return if @config == @prior
+        @repositories << Repository.new(name, tags, pattern)
+      end
 
-  @version = File.read(VERSION_PATH).chomp
+      def setup
+        unless @access_token
+          raise RuntimeError.new('HubSlurp requires an access_token.')
+        end
 
-  # Log4r
+        if @repositories.empty?
+          raise RuntimeError.new('You must specify at least one repository.')
+        end
 
-  @logger = Log4r::Logger.new 'default'
-  @logger.outputters = Log4r::Outputter.stdout
-  @logger.level = Log4r.const_get(@config['log_level'] || 'INFO')
+        @octokit = Octokit::Client.new access_token: @access_token
+      end
 
-  # Fog
+      def next_events
+        events = []
+        ts = Time.now
 
-  @logger.info "Authenticating to Rackspace as #{@config['rackspace_username']}."
-  service = Fog::Rackspace::Queues.new(
-    rackspace_username: @config['rackspace_username'],
-    rackspace_api_key: @config['rackspace_api_key'],
-    rackspace_region: @config['rackspace_region']
-  )
+        @repositories.each do |r|
+          logger.info "Polling for issues in #{repo.name} since #{@last_check}."
 
-  @q = service.queues.get(@config['queue_name'])
-  if @q.nil?
-    @q = service.queues.create(key: @config['queue_name'])
-    @logger.info "Created new queue #{@config['queue_name']}."
-  else
-    @logger.info "Found existing queue #{@config['queue_name']}."
-  end
+          @octokit.list_issues(repo.name, since: @last_check).each do |issue|
+            # Check for a keyword match.
+            if keyword_pattern =~ issue.title || keyword_pattern =~ issue.body
+              logger.info "#{repo.name}##{issue.number} contains a keyword match."
+              events << Event.new(
+                reporter: 'hubslurp',
+                url: issue.html_url,
+                title: issue.title,
+                origin_id: issue.id,
+                incident_date: issue.created_at.to_i,
+                tags: repo.tags
+              )
+            else
+              logger.debug "No hit in #{repo.name}##{issue.number}."
+            end
+          end
+        end
 
-  # Octokit
+        @last_check = ts
 
-  @octokit = Octokit::Client.new access_token: @config['github_access_token']
-
-  # Repositories
-
-  @repositories = @config['repositories'].map do |hash|
-    Repository.from_h hash
-  end
-
-  # Remember this configuration so we don't re-authenticate unnecessarily.
-  @prior = @config
-end
-
-configure
-
-# Main loop
-
-@logger.info "Launching HubSlurp version #{@version}."
-@logger.debug 'Debug logging is enabled.'
-
-scheduler = Rufus::Scheduler.new
-
-scheduler.interval(@config['interval'] || '5m') do
-  @logger.debug 'Awake again.'
-
-  ts = Time.now
-  if @last_check.nil?
-    if File.exist? LAST_PATH
-      @last_check = Time.at(File.read(LAST_PATH).to_i)
-    else
-      # Default to a day ago.
-      @last_check = ts - 86400
-    end
-  end
-
-  configure
-
-  keyword_pattern = Regexp.new(
-    @config['keywords'].map { |kw| Regexp.escape kw }.join('|'),
-    Regexp::IGNORECASE
-  )
-
-  @repositories.each do |repo|
-    @logger.info "Polling for issues in #{repo.name} since #{@last_check}."
-
-    @octokit.list_issues(repo.name, since: @last_check).each do |issue|
-      # Check for keyword matches.
-      if keyword_pattern =~ issue.title || keyword_pattern =~ issue.body
-        @logger.info "#{repo.name}##{issue.number} contains a keyword match."
-
-        # Publish an event.
-        @q.enqueue(
-          {
-            reporter: "hubslurp-#{@version}",
-            url: issue.url,
-            title: issue.title,
-            origin_id: issue.id,
-            incident_date: issue.created_at.to_i,
-            tags: repo.tags
-          },
-          3600
-        )
-      else
-        @logger.debug "No hit in #{repo.name}##{issue.number}."
+        events
       end
     end
+
   end
-
-  @last_check = ts
-  File.write(LAST_PATH, @last_check.to_i.to_s)
-
-  @logger.debug "Rate limit remaining: #{@octokit.rate_limit.remaining}"
-  @logger.debug 'Back to sleep.'
 end
-
-scheduler.join
