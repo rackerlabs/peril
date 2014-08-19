1503f4f8ec43bbca0281c5c200704e04f2108845
Ash Wilson <smashwilson@gmail.com>
2014-08-19 15:44:06 -0400

    Fetch recent Incidents.

--- lib/peril/web.rb
+++ lib/peril/web.rb
@@ -1,11 +1,38 @@
 # Global webhooks.
 
+require 'json'
+
 module Peril
   module Web
 
     def self.included(sinatra)
       sinatra.get '/' do
         'Online and ready to receive.'
+      end
+
+      sinatra.get '/incidents' do
+        unless params[:since]
+          halt 400, "You're missing the required 'since' parameter."
+        end
+        since = Time.at(params[:since].to_i)
+
+        headers 'Content-Type' => 'application/json'
+
+        Incident.where('updated_at > ?', since).order(:updated_at).map do |i|
+          {
+            unique_id: i.unique_id,
+            original_reporter: i.original_reporter,
+            url: i.url,
+            title: i.title,
+            tags: (i.tags || '').split(/\s*,\s*/),
+            assignee: i.assignee,
+            assigned_at: i.assigned_at.to_i,
+            completed_at: i.completed_at.to_i,
+            extra: i.extra,
+            created_at: i.created_at.to_i,
+            updated_at: i.updated_at.to_i
+          }
+        end.to_json
       end
     end
 
