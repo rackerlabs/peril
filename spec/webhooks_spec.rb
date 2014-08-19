7dca3e794668e376b1080e3df14ee6efd9a01759
Ash Wilson <smashwilson@gmail.com>
2014-08-19 15:17:03 -0400

    Test for listing incidents.

--- spec/webhooks_spec.rb
+++ spec/webhooks_spec.rb
@@ -2,6 +2,7 @@ require_relative 'spec_helpers'
 
 require 'sinatra/base'
 require 'rack/test'
+require 'json'
 
 class WebSlurper < Slurper
   def initialize
@@ -31,6 +32,22 @@ describe 'Webhooks' do
       last_response.must_be :ok?
       last_response.body.must_equal 'yep'
     end
+  end
+
+  describe 'incident listing' do
+    it 'lists recently updated incidents' do
+      ts = Time.at(1408475116)
+
+      Incident.create! unique_id: 'aaa', original_reporter: 'minitest', created_at: ts - 10, updated_at: ts - 10
+      Incident.create! unique_id: 'bbb', original_reporter: 'minitest', created_at: ts, updated_at: ts + 10
+      Incident.create! unique_id: 'ccc', original_reporter: 'minitest', created_at: ts - 10, updated_at: ts + 20
+
+      get '/incidents', since: ts.to_i
+
+      last_response.must_be :ok?
+      doc = JSON.parse(last_response.body)
+    end
+
   end
 
 end
